ce0dfc9cf2ce1db787dfd4a57f16b56b4cd43908
fredoggy <fredoggy@e70ac585-c325-7b89-82b1-460b150e6663>
2011-06-07 12:09:36 +0000

    Movements (nog niet af)

---
+++ trunk/movements.py
@@ -0,0 +1,9 @@
+def handle_movement(type, *args):
+   handlers = (forward:     go_forward,
+               left:        go_left,
+               right:       go_right,
+               reverse:     go_reverse
+               brake:       go_brake
+               rotate_left: go_rotate_left
+               rorate_right:go_rorate
+               
