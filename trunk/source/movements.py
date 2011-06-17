82c8ddb4db322ad52eae0e404f023516b8e07130
fredoggy <fredoggy@e70ac585-c325-7b89-82b1-460b150e6663>
2011-06-17 10:04:46 +0000

    movements, wallsearching_laser, werkuren

--- trunk/source/movements.py
+++ trunk/source/movements.py
@@ -1,38 +1,34 @@
 import string
 import socket
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 2002
-BUFFER_SIZE = 1024
-COLOR = ['Red', 'Yellow', 'Green', 'Cyan', 'White', 'Blue', 'Purple']
-TYPES = ['SonarSensor', 'SICKL-MS', 'OdometrySensor']
-NAMES = ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'Scanner1', 'Odometry']
-OPCODE = ['RESET', 'NOP']
+COLOR       = ['Red', 'Yellow', 'Green', 'Cyan', 'White', 'Blue', 'Purple']
+TYPES       = ['SonarSensor', 'SICKL-MS', 'OdometrySensor']
+NAMES       = ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'Scanner1', 'Odometry']
+OPCODE      = ['RESET', 'NOP']
 OPCODE_RFID = ['Release', 'Read', 'Write']
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send("INIT {ClassName USARBot.P2DX} {Location 4.5,1.9,1.8} {Name R1}\r\n")
 
+# The movement handler contains all sorts of possible movements
 def handle_movement(type, *args):
-   handlers = {"forward":         go_drive,
-               "left":            go_drive,
-               "right":           go_drive,
-               "reverse":         go_drive,
-               "brake":           go_drive,
-               "rotate_left":     go_drive,
-               "rotate_right":    go_drive,
-               "rotate_robot":    go_rotate,
-               "light":           go_light,
-               "camera":          go_camera,
-               "trace":           go_trace,
-               "sonar":           go_sensor,
-               "laser":           go_sensor,
-               "odometry":        go_sensor,
-               "rfid-tag":        go_tag,
-               "rfid":            go_rfid,
-              }
-   return handlers[type](*args)
-    
+    handlers = {"forward":         go_drive,
+                "left":            go_drive,
+                "right":           go_drive,
+                "reverse":         go_drive,
+                "brake":           go_drive,
+                "rotate_left":     go_drive,
+                "rotate_right":    go_drive,
+                "rotate_robot":    go_rotate,
+                "light":           go_light,
+                "camera":          go_camera,
+                "trace":           go_trace,
+                "sonar":           go_sensor,
+                "laser":           go_sensor,
+                "odometry":        go_sensor,
+                "rfid-tag":        go_tag,
+                "rfid":            go_rfid,
+               }
+    return handlers[type](*args)
+
+# Method to drive forward and backwards, make turns and rotations
 def go_drive(s1, s2):
     s1 = str(s1)
     s2 = str(s2)
@@ -40,12 +36,14 @@ def go_drive(s1, s2):
     print string
     return string
 
+# Method to make rotations
 def go_rotate(s1):
     s1 = str(s1)
     string = "DRIVE {RotationalVelocity " + s1 + "}"
     print string
     return string
 
+# Method to turn on and turn off the light
 def go_light(lights):
     if lights == 1:
        string = "DRIVE {Light true}\r\n"
@@ -53,7 +51,8 @@ def go_light(lights):
        string = "DRIVE {Light false}\r\n"
     print string
     return string
-
+   
+# Method to set the camera
 def go_camera(CameraPanTilt_Link2):
     if CameraPanTilt_Link2 == 1:
        string = "SET {Type Camera} {Name CameraPanTilt_Link2} {FOV 1}\r\n"
@@ -62,6 +61,7 @@ def go_camera(CameraPanTilt_Link2):
     print string
     return string
 
+# Method to leave dots used for tracing
 def go_trace(b1, in1, COLOR):
     b1 = str(b1)
     in1 = str(in1)
@@ -69,17 +69,20 @@ def go_trace(b1, in1, COLOR):
     print string
     return string
 
+# Method to control the sensor
 def go_sensor(TYPES, NAMES, OPCODE, params):
     params = str(params)
     string = "SET {Type " + TYPES + "} {Name " + NAMES + "} {Opcode " + OPCODE + "} {Params " + params + "}\r\n"
     print string
     return string
 
+# Method to drop a tag
 def go_tag(OPCODE_RFID):
     string = "SET {Type RFIDReleaser} {Name Gun} {Opcode " + OPCODE_RFID + "}\r\n"
     print string
     return string
 
+# Method to read, write and erase RFID tags
 def go_rfid(OPCODE_RFID, RFIDTagID, MemoryContent):
     MemoryContent = str(MemoryContent)
     string = " "
@@ -89,20 +92,4 @@ def go_rfid(OPCODE_RFID, RFIDTagID, MemoryContent):
         elif OPCODE_RFID == 'Read':
             string = "SET {Type RFID} {Name RFID} {Opcode " + OPCODE_RFID + "} {Params " + RFIDTagID[i] + "}\r\n"
     print string
-    return string  
-
-while 1:
-   s.send(handle_movement("camera", 1))
-   s.send(handle_movement("rfid-tag", 'Release'))
-   s.send(handle_movement("trace", 1, 1, 'White'))
-   s.send(handle_movement("forward", 5.0, 5.0))
-   #s.send(handle_movement("rotate_left", 1.0, -1.0))
-   #s.send(handle_movement("rotate_right", -1.0, 1.0))
-   #s.send(handle_movement("brake", 0.0, 0.0))
-   s.send(handle_movement("light", 1))
-   s.send(handle_movement("sonar", 'SonarSensor', 'F3', 'RESET', 5))
-   #s.send(handle_movement("rfid", 'Write', id_value, 0))
-
-s.close()
-    
-               
+    return string
--- trunk/test/Muurzoeker/wallsearching_laser.py
+++ trunk/test/Muurzoeker/wallsearching_laser.py
@@ -9,8 +9,8 @@ COLOR = ['Red', 'Yellow', 'Green', 'Cyan', 'White', 'Blue', 'Purple']
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 s.send("INIT {ClassName USARBot.P2DX} {Location -1.0,1.9,1.8} {Name R1}\r\n")
-#s.send("INIT {ClassName USARBot.P2DX} {Location 4.3,1.1,1.8} {Name R1}\r\n")
 
+# The partial movement handler contains all sorts of possible movements
 def handle_movement(type, *args):
    handlers = {"forward":         go_drive,
                "left":            go_drive,
@@ -19,172 +19,157 @@ def handle_movement(type, *args):
                "brake":           go_drive,
                "rotate_left":     go_drive,
                "rotate_right":    go_drive,
-               "trace":           go_trace,
                "rotate_robot":    go_rotate,
               }
    return handlers[type](*args)
 
+# Method to drive forward and backwards, make turns and rotations
 def go_drive(s1, s2):
     s1 = str(s1)
     s2 = str(s2)
     string = "DRIVE {Left " + s1 + "} {Right " + s2 + "}\r\n"
-##    print string
     return string
 
-def go_trace(b1, in1, COLOR):
-    b1 = str(b1)
-    in1 = str(in1)
-    string = "Trace {On " + b1 + "} {Interval " + in1 + "} {Color " + COLOR +"}\r\n"
-##    print string
-    return string
-
+# Method to make rotations
 def go_rotate(s1):
     s1 = str(s1)
     string = "DRIVE {RotationalVelocity " + s1 + "}"
-##    print string
     return string
 
+# Method to convert strings to floats
 def string_to_float(vals):
     float_vals = []
     for i in range(len(vals)):
         float_vals.append(float(vals[i]))
     return float_vals     
 
+# Method to find the minimum value and its index value
 def min_laser_val(laser_vals):
     laser_vals = string_to_float(laser_vals)
     sorted_laser_vals = sorted(laser_vals)
     index_val = laser_vals.index(sorted_laser_vals[0]) + 1
-    #print sorted_laser_vals[0]
-    #print index_val
     return sorted_laser_vals[0], index_val
 
+# Method to retrieve the list of odometry values
 def odometry_module(datastring):
     senvalues = datastring[3].replace('{Pose ', '')
     senvalues = senvalues.replace('}','')
     odo_values = senvalues.split(',')
     return odo_values
 
+# Method to make a full rotation to determine the nearest wall in the robot its environment
 def turn_360(odo_values):
-   x = odo_values[0]
-   y = odo_values[1]
-   theta = odo_values[2]
-   s.send(handle_movement("rotate_left", -1.5, 1.0))
-   new_odo_values = [999, 999, 999]
-   temp_min_val = 10000
-   temp_index_val = 0
-   temp_odo_values = [999, 999, 999]
-   previous_odo_values = [999, 999, 999]
-   flag = 0
-   while(1):
-      data = s.recv(BUFFER_SIZE)
-      string = data.split('\r\n')
-      sonar_values = []
-      #print temp_odo_values
-      for i in range(len(string)):
-          datasplit = re.findall('\{[^\}]*\}|\S+', string[i]) 
-          if len(datasplit) > 0:
-              # Sensor message
-              if datasplit[0] == "SEN":
-                  #print datasplit, "\r\n"
-                  typeSEN = datasplit[1].replace('{Type ', '')
-                  typeSEN = typeSEN.replace('}', '')
-                  # Odometry sensor
-                  if typeSEN == "Odometry":
-                      #print datasplit
-                      new_odo_values = string_to_float(odometry_module(datasplit))
-                      #print new_odo_values, "\r\n"
-                      if new_odo_values[2] > previous_odo_values[2]:
-                         if flag == 2:
-                             s.send(handle_movement("brake", 0.0, 0.0))
-                             print "De kleinste min value gevonden"
-                             turn_right_position(temp_min_val, temp_index_val, temp_odo_values)
-                             return #placeholder wallfollow
-                         flag = 1
-                      if flag == 1 and new_odo_values[2] <  0:
-                         flag = 2
-                      previous_odo_values = new_odo_values
-                  if len(datasplit) > 2:
-                       typeSEN2 = datasplit[2].replace('{Type ', '')
-                       typeSEN2 = typeSEN2.replace('}', '')
-                       # Range sensor
-                       if typeSEN2 == "RangeScanner":
-                           #print datasplit, "\r\n"
-                           if len(datasplit) > 7:
-                               laser_values = re.findall('([\d.]*\d+)', datasplit[7])
-                               #print laser_values
-                               #print len(laser_values)
-                               min_val, index_val = min_laser_val(laser_values)
-                               print min_val
-                               middle = len(laser_values)/2
-                               treshold = len(laser_values)/20
-                               if index_val <= middle+treshold and index_val >= middle -treshold:
-                                   if min_val < temp_min_val:
-                                       print min_val
-                                       print index_val
-                                       print 'minimum waarde'
-                                       temp_min_val = min_val
-                                       temp_index_val = index_val
-                                       temp_odo_values = new_odo_values
+    x = odo_values[0]
+    y = odo_values[1]
+    theta = odo_values[2]
+    new_odo_values = [999, 999, 999]
+    temp_min_val = 10000
+    temp_index_val = 0
+    temp_odo_values = [999, 999, 999]
+    previous_odo_values = [999, 999, 999]
+    flag = 0
+    
+    s.send(handle_movement("rotate_left", -1.5, 1.0))
+    while(1):
+        data = s.recv(BUFFER_SIZE)
+        string = data.split('\r\n')
+        sonar_values = []
+        for i in range(len(string)):
+            datasplit = re.findall('\{[^\}]*\}|\S+', string[i]) 
+            if len(datasplit) > 0:
+                # Sensor message
+                if datasplit[0] == "SEN":
+                    typeSEN = datasplit[1].replace('{Type ', '')
+                    typeSEN = typeSEN.replace('}', '')
+                    # Odometry sensor
+                    if typeSEN == "Odometry":
+                        new_odo_values = string_to_float(odometry_module(datasplit))
+                        if new_odo_values[2] > previous_odo_values[2]:
+                            if flag == 2:
+                                s.send(handle_movement("brake", 0.0, 0.0))
+                                print "Of all minimun values the smallest one is found"
+                                turn_right_position(temp_min_val, temp_index_val, temp_odo_values)
+                                return
+                            flag = 1
+                        if flag == 1 and new_odo_values[2] < 0:
+                            flag = 2
+                        previous_odo_values = new_odo_values
+                    if len(datasplit) > 2:
+                        typeSEN2 = datasplit[2].replace('{Type ', '')
+                        typeSEN2 = typeSEN2.replace('}', '')
+                        # Laser sensor
+                        if typeSEN2 == "RangeScanner":
+                            if len(datasplit) > 7:
+                                laser_values = re.findall('([\d.]*\d+)', datasplit[7])
+                                min_val, index_val = min_laser_val(laser_values)
+                                print min_val
+                                # Determine the middle of the number of values and a threshold
+                                middle = len(laser_values) / 2
+                                treshold = len(laser_values) / 20
+                                if index_val <= middle + treshold and index_val >= middle - treshold:
+                                    if min_val < temp_min_val:
+                                        print min_val
+                                        print index_val
+                                        print 'minimum value'
+                                        temp_min_val = min_val
+                                        temp_index_val = index_val
+                                        temp_odo_values = new_odo_values
 
+# Method to rotate to the determined position
 def turn_right_position(min_val, index_val, odo_values):
-   s.send(handle_movement("rotate_left", -1.5, 1.0))
-   new_odo_values = [999, 999, 999]
-   previous_odo_values = [999, 999, 999]
-   flag = 0
-   print "ik ga nu naar de goede waarde draaien"
-   while(1):
-      data = s.recv(BUFFER_SIZE)
-      string = data.split('\r\n')
-      sonar_values = []
-      for i in range(len(string)):
-          datasplit = re.findall('\{[^\}]*\}|\S+', string[i]) 
-          if len(datasplit) > 0:
-              # Sensor message
-              if datasplit[0] == "SEN":
-                  #print datasplit, "\r\n"
-                  typeSEN = datasplit[1].replace('{Type ', '')
-                  typeSEN = typeSEN.replace('}', '')
-                  # Odometry sensor
-                  if typeSEN == "Odometry":
-                      #print datasplit
-                      new_odo_values = string_to_float(odometry_module(datasplit))
-                      if(new_odo_values[2] < odo_values[2]):
-                          s.send(handle_movement("brake", 0.0, 0.0))
-                          print "De juiste positie gevonden"
-                          s.send(handle_movement("forward", 1.0, 1.0))
-                          wallsearch()
-                          print " turn to the right position",index_val
-                          return
+    s.send(handle_movement("rotate_left", -1.5, 1.0))
+    new_odo_values = [999, 999, 999]
+    previous_odo_values = [999, 999, 999]
+    flag = 0
+    print "Robot is going to rotate to the right position"
+    while(1):
+        data = s.recv(BUFFER_SIZE)
+        string = data.split('\r\n')
+        sonar_values = []
+        for i in range(len(string)):
+            datasplit = re.findall('\{[^\}]*\}|\S+', string[i]) 
+            if len(datasplit) > 0:
+                # Sensor message
+                if datasplit[0] == "SEN":
+                    typeSEN = datasplit[1].replace('{Type ', '')
+                    typeSEN = typeSEN.replace('}', '')
+                    # Odometry sensor
+                    if typeSEN == "Odometry":
+                        new_odo_values = string_to_float(odometry_module(datasplit))
+                        if new_odo_values[2] < odo_values[2]:
+                            s.send(handle_movement("brake", 0.0, 0.0))
+                            print "The right position is found"
+                            s.send(handle_movement("forward", 1.0, 1.0))
+                            wallsearch()
+                            print "Move to the right position", index_val
+                            return
 
+# Move to the nearest wall
 def wallsearch():
-    print 'ik ben hier ik wil stoppen'
-    min_val =  100000000
+    min_val = 100000000
     while 1:
-       data = s.recv(BUFFER_SIZE)
-       string = data.split('\r\n')
-       laser_values = []
-       for i in range(len(string)):
-           datasplit = re.findall('\{[^\}]*\}|\S+', string[i]) 
-           if len(datasplit) > 0:
-               # Sensor message
-               if datasplit[0] == "SEN":
-                   #print datasplit, "\r\n"
-                   typeSEN = datasplit[2].replace('{Type ', '')
-                   typeSEN = typeSEN.replace('}', '')
-                   if typeSEN == "RangeScanner":
-                      #print datasplit, "\r\n"
-                      if len(datasplit) > 7:                         
-                         laser_values = re.findall('([\d.]*\d+)', datasplit[7])
-##                         print laser_values
-                         min_val, index_val = min_laser_val(laser_values)
-       print "minval in wallseatch ",min_val
-       if min_val <= 0.3:
-           print "De muur gevonden"
-           s.send(handle_movement("brake", 0.0, 0.0))
-           break
-                                         
+        data = s.recv(BUFFER_SIZE)
+        string = data.split('\r\n')
+        laser_values = []
+        for i in range(len(string)):
+            datasplit = re.findall('\{[^\}]*\}|\S+', string[i]) 
+            if len(datasplit) > 0:
+                # Sensor message
+                if datasplit[0] == "SEN":
+                    typeSEN = datasplit[2].replace('{Type ', '')
+                    typeSEN = typeSEN.replace('}', '')
+                    # Laser sensor
+                    if typeSEN == "RangeScanner":
+                        if len(datasplit) > 7:                         
+                            laser_values = re.findall('([\d.]*\d+)', datasplit[7])
+                            min_val, index_val = min_laser_val(laser_values)
+        print "Min value in wallsearch ", min_val
+        if min_val <= 0.3:
+            print "The wall has been found"
+            s.send(handle_movement("brake", 0.0, 0.0))
+            break
+
 while(1):
-    print "ik ben nu weer hier"
     data = s.recv(BUFFER_SIZE)
     string = data.split('\r\n')
     sonar_values = []
@@ -194,15 +179,13 @@ while(1):
         if len(datasplit) > 0:
             # Sensor message
             if datasplit[0] == "SEN":
-                #print datasplit, "\r\n"
                 typeSEN = datasplit[1].replace('{Type ', '')
                 typeSEN = typeSEN.replace('}', '')
                 # Odometry sensor
                 if typeSEN == "Odometry":
                     odo_values = string_to_float(odometry_module(datasplit))
-                    #print odo_values, "\r\n"
                     turn_360(odo_values)
                     while 1:
                         if flag != 1:
-                           print 'ik ben klaar'
-                           flag = 1
+                            print 'Robot is done'
+                            flag = 1
