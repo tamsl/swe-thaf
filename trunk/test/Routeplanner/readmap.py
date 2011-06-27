de091cf029b3688ed46ef7bba4ffa37b143fe0e5
fredoggy <fredoggy@e70ac585-c325-7b89-82b1-460b150e6663>
2011-06-27 10:21:54 +0000

    Read map

--- trunk/test/Routeplanner/readmap.py
+++ trunk/test/Routeplanner/readmap.py
@@ -1,19 +1,71 @@
 def read_map(matrix):
+    route = []
     for i in range(len(matrix)):
         for k in range(len(matrix)):
             if matrix[i][k] == 'S':
                 xs = i
                 ys = k
-            if matrix[i][k] == 'F':
+            elif matrix[i][k] == 'F':
                 xf = i
-                yf = k
-            if matrix[i][k] == 'R':
-                
+                yf = k 
     print "S", xs, ys
     print "F", xf, yf
+    x = xs
+    y = ys
+    flag = 1
+    new_angle = 0
+    old_angle = -1
+    route.append([x, y])
+    while flag == 1:
+        for i in range(x - 1, x + 2):
+            for j in range(y - 1, y + 2):
+                if i < x and j < y:
+                    new_angle = 1
+                elif i < x and j == y:
+                    new_angle = 2
+                elif i < x and j > y:
+                    new_angle = 3
+                elif i == x and j < y:
+                    new_angle = 4
+                elif i == x and j > y:
+                    new_angle = 6
+                elif i > x and j < y:
+                    new_angle = 7
+                elif i > x and j == y:
+                    new_angle = 8
+                elif i > x and j > y:
+                    new_angle = 9
+                if matrix[i][j] == 'R':
+                    if i != x or j != y:
+                        matrix[i][j] = 0
+                        if old_angle == new_angle:
+                            route.pop()
+                        old_angle = new_angle
+                        x = i
+                        y = j
+                        route.append([x, y])
+                elif matrix[i][j] == 'F':
+                    if old_angle == new_angle:
+                       route.pop()
+                    route.append([xf, yf])
+                    flag = 0
+    return route
     
+matrix = [[0, 'R', 'R', 0, 0], ['R', 0, 0, 'R', 0], ['S', 0, 0, 'R', 0], [0, 0, 0, 'R', 0], [0, 0, 0, 'F', 0]]
 
-matrix = [['S', 2, 3],['R', 5, 6],['R', 'R', 'F']]
-print len(matrix)
-read_map(matrix)
+## display the map with the route added
+print 'Map:'
+for y in range(len(matrix)):
+    for x in range(len(matrix)):
+        xy = matrix[y][x]
+        if xy == 0:
+            print '.', # space
+        elif xy == 'S':
+            print 'S', # start
+        elif xy == 'R':
+            print 'R', # route
+        elif xy == 'F':
+            print 'F', # finish
+    print
 
+print read_map(matrix)
