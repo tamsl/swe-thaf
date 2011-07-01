121094f100935308e7702c3246acfc1fe55be0b2
fredoggy <fredoggy@gmail.com>
2011-07-01 07:40:14 +0000

    Changes

--- test/Routeplanner/strings.py
+++ test/Routeplanner/strings.py
@@ -1,35 +1,49 @@
 import math
-
+ 
 # Method to compress a matrix into a string.
 def compress_matrix(matrix):
-    how_many = 1
+    how_many = 0
     old_value = -1
     string = ""
+ 
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if matrix[i][j] == 0:
-                if matrix[i][j] == old_value:
+                if matrix[i][j] == old_value or (matrix[i][j] != old_value and i == 0 and j == 0):
                     how_many = how_many + 1
                 elif matrix[i][j] != old_value:
-                    string = string + "_" + str(how_many) + "1"
+                    if string == "":
+                        string = str(how_many) + "1"
+                    else:
+                        string = string + "_" + str(how_many) + "1"
                     how_many = 1
             elif matrix[i][j] == 1:
-                if matrix[i][j] == old_value:
-                    how_many = how_many + 1
+                if matrix[i][j] == old_value or (matrix[i][j] != old_value and i == 0 and j == 0):
+                    how_many = how_many + 1            
                 elif matrix[i][j] != old_value:
-                    string = string + "_" + str(how_many) + "0"
+                    if string == "":
+                        string = str(how_many) + "0"
+                    else:
+                        string = string + "_" + str(how_many) + "0"
                     how_many = 1
             old_value = matrix[i][j]
+ 
+    # The last number of values.
+    if old_value == '0':
+        string = string + "_" + str(how_many) + "1"
+    else:
+        string = string + "_" + str(how_many) + "0"
     return string
-
+ 
 # Method to decompress the string into a matrix.
-def receive_compressed_into_matrix(string, row_length):
+def decompress_string(string, row_length):
     index = 0
     count = 0
     row_number = 0
     matrix = []
     line = []
     string = string.split('_')
+ 
     for i in range(len(string)):
         how_many = string[i][:-1]
         value = string[i][len(string[i]) - 1]
@@ -43,40 +57,47 @@ def receive_compressed_into_matrix(string, row_length):
             matrix.append(line)
             line = []
     return matrix
-
-##def create_int_matrix(matrix):
-##    x , y = matrix.shape
-##    for i in range (x):
-##        for j in range (y):
-##             = matrix[i][j]
-            
-##string = "5001_5000_"
-##string2 = "" 
-##for i in range(1000):
-##    string2 = string2 + string
-##string2 = string2[:-1]
-##test = receive_compressed_into_matrix(string2, 1000)
-##print test
-##print compress_matrix(test)
-
-##string2 = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000011111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111110000000000000000000000000000000000000000000000000000000000000000000000000000000000000000111111111111111100000000000000000000000000000000000000000000000001111111111111111111111111111111111111111111111111111111111111111111111111111111110000000000000000000000000000000000000000000000000000000000000000000000000000000000011111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111000000000000000000000000000000000000000000000000000000000000000000000000000000011111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111100000000000000000000000000000000000000000000000000000000000000000001000000000010101010101010100010101010101000000000000000000000111111111111"
-##string3 = ""
-##for i in range(1000):
-##    string3 = string3 + string2
-##test2 = divide_string(string3, 999, 1000)
-##matrix = create_matrix(test2)
-##downsized_matrix = downsize_matrix(matrix)
-##
-##print len(downsized_matrix)
-##
-##print 'Map:'
-##for y in range(200):
-##    for x in range(200):
-##        xy = downsized_matrix[y][x]
-##        if xy == '1':
-##            print '.', # space
-##        elif xy == '0':
-##            print 'O', # obstacle
-
-
-
+ 
+# Test both methods on their functionality.            
+partial = "5001_5000_"
+string = ""
+ 
+# Generate a string that if decompressed a 1000 by 1000 matrix appears. 
+for i in range(1000):
+    string = string + partial
+string = string[:-1]
+ 
+# A 1000 by 1000 matrix results from a string that has been decompressed.
+matrix = decompress_string(string, 1000)
+ 
+# Compress that matrix to a string.
+string2 = compress_matrix(matrix)
+ 
+# Check if the string before decompression and compression equals the
+# one after decompression and compression.
+if string == string2:
+    print "\nstring and string2 are equal. The compress and decompress methods both work.\n"
+else:
+    print "\nstring and string2 are not equal. The compress and decompress methods don't work.\n"
+ 
+# Test again for another string.          
+partial = "2001_2000_501_500_3001_2000_"
+string = ""
+ 
+# Generate a string that if decompressed a 1000 by 1000 matrix appears. 
+for i in range(1000):
+    string = string + partial
+string = string[:-1]
+ 
+# A 1000 by 1000 matrix results from a string that has been decompressed.
+matrix = decompress_string(string, 1000)
+ 
+# Compress that matrix to a string.
+string2 = compress_matrix(matrix)
+ 
+# Check if the string before decompression and compression equals the
+# one after decompression and compression.
+if string == string2:
+    print "\nstring and string2 are equal. The compress and decompress methods both work.\n"
+else:
+    print "\nstring and string2 are not equal. The compress and decompress methods don't work.\n"
